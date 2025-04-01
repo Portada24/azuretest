210ba4b521e0a559380e109f0c43f82235be701c
Portada24 <123512224+Portada24@users.noreply.github.com>
2025-03-31 20:27:18 -0500

    mensaje cambiado

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('¡Hola desde Azure App Service, prueba creada por David 🥷! probando con la nube de azure');
+    res.send('¡Hola desde Azure App Service, prueba creada por David 🥷! probando el servicio de azure');
 });
 
 app.listen(port, () => {
