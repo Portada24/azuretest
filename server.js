12cac28ceefe7cd9de0ad2f1dfaa8c370ef324ac
Portada24 <123512224+Portada24@users.noreply.github.com>
2025-04-02 09:25:30 -0500

    mensaje con felipe

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('¡Hola desde Azure App Service, prueba creada por David 🥷! probando el servicio de azure');
+    res.send('¡Hola desde Azure App Service, prueba creada por David 🥷! probando el servicio de azure con Felipe');
 });
 
 app.listen(port, () => {
